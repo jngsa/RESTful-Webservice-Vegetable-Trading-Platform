e01847c7f202ba71d3f547f4beb28f5fd7cad2ca
braune1 <s59ebrau@uni-bonn.de>
2024-07-09 16:05:25 +0200

    merge vorb

--- extra/server.py
+++ extra/server.py
@@ -3,24 +3,16 @@ from typing import Dict
 from pydantic import BaseModel
 import uvicorn
 import os
+import time
+import threading
 import konto
 import bank
 import ware
 
 app = FastAPI()
 
-from fastapi.middleware.cors import CORSMiddleware
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Erlaubt alle Ursprünge
-    allow_credentials=True,
-    allow_methods=["*"],  # Erlaubt alle Methoden
-    allow_headers=["*"],  # Erlaubt alle Header
-)
-
 konto_liste: Dict[str, konto.Konto] = {} # Verzeichnis aller existierenden Kontos
-bank_existenz = []
+bank_existenz = []  # enthält die aktuelle Bank
 
 class meineWare(BaseModel):
     """Modell für Handelsgut/Ware"""
@@ -28,24 +20,60 @@ class meineWare(BaseModel):
     price: float
     units: int
 
+# -------------------------------------------------------------------------------
+# Preis-Aktualisieren (jede 30 Sekunden)
+
+def update_prices():
+    while True:
+        if len(bank_existenz) == 0:
+            aktuelle_bank = bank.Bank()             # die Bank initialisieren
+            bank_existenz.append(aktuelle_bank)     # diese Bank in bank_existenz einfügen
+        
+        else:
+            aktuelle_bank = bank_existenz[0]
+        
+        time.sleep(30)
+        aktuelle_bank.updatePrices()
+        print("STATUS UPDATE: Preise von Waren aktualisiert.")
+
+# Preis-Aktualisieren im Hintergrund ausführen
+def start_background_tasks():
+    thread = threading.Thread(target=update_prices, daemon=True)
+    thread.start()
+
+@app.on_event("startup")
+async def startup_event():
+    start_background_tasks()
+
+# -------------------------------------------------------------------------------
+
 @app.post("/create_konto/")
 def create_konto(benutzername : str, passwort : str):
     """Erstellt ein neues Konto mit eingegebenem Benutzernamen & Passwort"""
-    neues_konto = konto.Konto(benutzername, passwort)
-    konto_liste[benutzername] = neues_konto
+    
+    if benutzername in konto_liste:     # falls andere(r) Nutzer*in mit dem Benutzernamen schon angemeldet hat
+        return {
+            "message" : "Dieser Benutzername ist belegt.",
+            "benutzername" : ""
+        }
+
+    neues_konto = konto.Konto(benutzername, passwort)   # ein Konto erstellen
+    konto_liste[benutzername] = neues_konto             # fügt dieses Konto in konto_liste ein
     return {
-        "message" : "✓✓✓ Das Konto wurde erfolgreich erstellt. ✓✓✓",
+        "message" : "Das Konto wurde erfolgreich erstellt. ",
         "benutzername" : benutzername
     }
 
 @app.post("/login/")
 def login(benutzername : str, passwort : str):
     """Loggt ein Nutzer*in mit eingegebenem Benutzernamen & Passwort ein"""
+
+    # prüft, ob das Konto mit diesem Benutzernamen schon existiert
     if benutzername in konto_liste:
         aktuelles_konto = konto_liste[benutzername]
-        if aktuelles_konto.getPasswort() == passwort:
+        if aktuelles_konto.getPasswort() == passwort:   # Einloggen mit dem Passwort authentifizieren
             return {
-                "message" : "✓✓✓ Einloggen erfolgreich. Wilkommen zurück! ✓✓✓",
+                "message" : "Einloggen erfolgreich. Wilkommen zurück!",
                 "benutzername" : benutzername
             }
         
@@ -58,19 +86,20 @@ def login(benutzername : str, passwort : str):
 @app.post("/start/")
 def start():
     """die Bank initialisieren, falls noch nicht"""
-    if len(bank_existenz) == 0:
-        aktuelle_bank = bank.Bank()
-        bank_existenz.append(aktuelle_bank)
+
+    if len(bank_existenz) == 0:                 # falls die Bank noch nicht initialisiert wird
+        aktuelle_bank = bank.Bank()             # die Bank initialisieren
+        bank_existenz.append(aktuelle_bank)     # diese Bank in bank_existenz einfügen
 
         return {
-        "message" : "✓✓✓ die Bank wurde erfolgreich initialisiert ✓✓✓",
+        "message" : "STATUS UPDATE: Die Bank wurde erfolgreich initialisiert.",
         }
     
     else:
-        aktuelle_bank = bank_existenz[0]
+        aktuelle_bank = bank_existenz[0]        # falls die Bank schon initialisiert wurde, tut nichts
 
         return {
-            "message" : "✓✓✓ die Bank funktioniert gerade ✓✓✓",
+            "message" : "STATUS UPDATE: Die Bank funktioniert gerade.",
         }
 
 @app.get("/mein_konto/")
@@ -80,12 +109,14 @@ def mein_konto(benutzername : str):
     if benutzername not in konto_liste:
         raise HTTPException(status_code=404, detail="Konto nicht gefunden")
     
-    aktuelles_konto = konto_liste[benutzername]
+    aktuelles_konto = konto_liste[benutzername]     # Holt existierendes Konto vom konto_liste ab
 
-    inventar = aktuelles_konto.getInventar()
-    myInventar: Dict[str, meineWare] = {}       # Verzeichnis aller besitzten Waren
+    inventar = aktuelles_konto.getInventar()        # Holt das Inventar des Kontos
+    myInventar: Dict[str, meineWare] = {}           # Verzeichnis aller besitzten Waren
     isEmpty = aktuelles_konto.istInventarEmpty()    # prüft, ob Inventar leer ist
 
+    # Wandeln std::unordered_map mit value class Ware in einen Dict mit value Base-Model meineWare um
+    # (class Ware kann nicht in json gepackt werden, deshalb ist diese Umwandlung nötig)
     for name, aktuelle_ware in inventar.items():
         data = {
             "ware_name" : name,
@@ -93,7 +124,7 @@ def mein_konto(benutzername : str):
             "units" : aktuelle_ware.getUnits()
         }
 
-        ware_instanz = meineWare(**data)
+        ware_instanz = meineWare(**data)        # data als Base-Model speichern
         myInventar[name] = ware_instanz         # Ware zum Inventar hinzufügen
 
     return {
@@ -110,10 +141,10 @@ def get_bank_waren():
     if len(bank_existenz) == 0:
         raise HTTPException(status_code=404, detail="Bank noch nicht initialisiert")
     
-    aktuelle_bank = bank_existenz[0]
+    aktuelle_bank = bank_existenz[0]        # Holt die Bank ab
     stocks = aktuelle_bank.getStocks()      # stocks enthält ware, die aus dem Namen, Preis und Units besteht  (Stocks -> Ware -> Name, Preis, Units)
     
-    # stocks from Source-Datei in ein Dict umwandeln
+    # stocks from Source-Datei in ein Dict umwandeln (ähnlicher Grund wie beim Inventar)
     
     bank_stocks: Dict[str, meineWare] = {}
 
@@ -124,8 +155,8 @@ def get_bank_waren():
             "units" : güter.getUnits()
         }
 
-        ware_instanz = meineWare(**data)
-        bank_stocks[güter.getName()] = ware_instanz
+        ware_instanz = meineWare(**data)            # data als Base-Model speichern
+        bank_stocks[güter.getName()] = ware_instanz # Ware zum bank_stocks hinzufügen
 
     return {
         "message" : "Alle Güter von der Bank erfolgreich zugegriffen",
@@ -139,14 +170,38 @@ def kaufen(benutzername : str, ware_name : str, units : int):
     if benutzername not in konto_liste:
         raise HTTPException(status_code=404, detail="Konto nicht gefunden")
     
-    aktuelles_konto = konto_liste[benutzername]
-    aktuelle_bank = bank_existenz[0]
+    try:
+        aktuelles_konto = konto_liste[benutzername]                 # Holt das Konto und die Bank ab
+        aktuelle_bank = bank_existenz[0]
 
-    aktuelles_konto.buyWare(aktuelle_bank, ware_name, units)    # Ware(n) von der Bank kaufen
-
+        aktuelles_konto.buyWare(aktuelle_bank, ware_name, units)    # Ware(n) von der Bank kaufen
+    
+    except konto.InvalidArgument as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    
     return {
-        "message" : "Kauf erfolgreich!"
-    }
+            "message" : "Kauf erfolgreich!"
+        }
+
+@app.post("/verkaufen/")
+def verkaufen(benutzername : str, ware_name : str, units : int):
+    """Verkauft eingegebene Ware"""
+
+    if benutzername not in konto_liste:
+        raise HTTPException(status_code=404, detail="Konto nicht gefunden")
+    
+    try:
+        aktuelles_konto = konto_liste[benutzername]                 # Holt das Konto und die Bank ab
+        aktuelle_bank = bank_existenz[0]
+
+        aktuelles_konto.sellWare(aktuelle_bank, ware_name, units)   # der Bank Ware(n) verkaufen
+    
+    except konto.InvalidArgument as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    
+    return {
+            "message" : "Verkauf erfolgreich!"
+        }
 
 
 if __name__ == "__main__":
